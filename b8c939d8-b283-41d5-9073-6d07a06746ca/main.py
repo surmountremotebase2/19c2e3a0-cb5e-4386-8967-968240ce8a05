b01bde326aca74b51860ba938d20652690e2ad6e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-12 16:50:56 -0400

    User Craig Woody saved changes to main.py at 03/12/2024, 20:50:56 UTC time.

--- b8c939d8-b283-41d5-9073-6d07a06746ca/main.py
+++ b8c939d8-b283-41d5-9073-6d07a06746ca/main.py
@@ -58,7 +58,7 @@ class TradingStrategy(Strategy):
             smavol40 = SMAVol(ticker, data["ohlcv"], 40)
             smavol5 = SMAVol(ticker, data["ohlcv"], 5)
 
-            if len(vols)==0 or smavol40 is None or smavol40 <= 0 or smavol5 is None or smavol5 <= 0:
+            if len(vols)==0 or smavol40[-1] is None or smavol40[-1] <= 0 or smavol5[-1] is None or smavol5[-1] <= 0:
                     self.allocation_dict[ticker] = 0
                     continue
                     
