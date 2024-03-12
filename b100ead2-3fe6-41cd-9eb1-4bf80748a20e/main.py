0fc23be8682fd8f675df3c6fc01ac74ea4f937b0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-12 13:37:02 -0400

    User Craig Woody saved changes to main.py at 03/12/2024, 17:37:01 UTC time.

--- b100ead2-3fe6-41cd-9eb1-4bf80748a20e/main.py
+++ b100ead2-3fe6-41cd-9eb1-4bf80748a20e/main.py
@@ -117,7 +117,7 @@ class TradingStrategy(Strategy):
             else:
                 allocation_dict[ticker] = 0  # Do not allocate if the condition doesn't meet
         
-        allocated_stocks = [ticker for ticker, value in allocation_dict[] if value == 1]
+        allocated_stocks = [x for x in allocation_dict if x == 1]
         
         # Calculate total number of allocated stocks
         total_allocated = len(allocated_stocks)
