a0358c94d51339948d00b1ec940e9a8f2d3e1927
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-12 13:08:46 -0400

    User Craig Woody saved changes to main.py at 03/12/2024, 17:08:45 UTC time.

--- b100ead2-3fe6-41cd-9eb1-4bf80748a20e/main.py
+++ b100ead2-3fe6-41cd-9eb1-4bf80748a20e/main.py
@@ -4,7 +4,7 @@ from surmount.data import Asset
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["AAPL", "GOOGL", "MSFT", "AMZN","ABT","AMD","NVDA","HD"]
+        self.tickers = ["AAPL", "MSFT", "AMZN", "GOOGL", "FB", "TSLA", "BRK.A", "BRK.B", "JNJ", "JPM", "NVDA", "V", "PG", "HD", "MA", "INTC", "UNH", "BAC", "DIS", "ADBE", "CRM", "PYPL", "NFLX", "CMCSA", "KO", "T", "PEP", "MRK", "XOM", "WMT", "CSCO", "ABT", "VZ", "NKE", "ABBV", "TMO", "CVX", "ACN", "MCD", "COST", "TXN", "NEE", "WFC", "HON", "ORCL", "LLY", "DHR", "PM", "QCOM", "AMGN", "UNP", "IBM", "AMD", "NOW", "AXP", "LIN", "CAT", "MDT", "GS", "LOW", "UPS", "SBUX", "RTX", "SPGI", "MMM", "BLK", "INTU", "DUK", "MO", "BDX", "ISRG", "CI", "CSX", "PLD", "VRTX", "TGT", "LMT", "ICE", "ADI", "ANTM", "SO", "BMY", "CME", "SYK", "ZTS", "SPG", "FIS", "REGN", "CHTR", "GILD", "ATVI", "MMC", "ADP", "CB", "SCHW", "BSX"]
         # Initialize a dictionary to track the holding status of each stock
         self.stock_holdings = {ticker: False for ticker in self.tickers}
 
@@ -22,25 +22,74 @@ class TradingStrategy(Strategy):
         
         for ticker in self.tickers:
             # Check if we have enough data points (at least 4 days to make a decision)
-            if len(data["ohlcv"]) < 4:
+            if len(data["ohlcv"]) < 10:
                 allocation_dict[ticker] = 0
                 continue
             
             ohlcv_data = data["ohlcv"]
             
-            # Check if the stock has been gaining for 3 consecutive days
-            gaining_for_5_days = ohlcv_data[-6][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
-                                 ohlcv_data[-5][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
+            # Check if the stock has been gaining or losing for consecutive days
+
+            gaining_for_5_days = ohlcv_data[-6][ticker]["close"] < ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] < ohlcv_data[-4][ticker]["close"] and \
                                  ohlcv_data[-4][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
                                  ohlcv_data[-3][ticker]["close"] < ohlcv_data[-2][ticker]["close"] and \
                                  ohlcv_data[-2][ticker]["close"] < ohlcv_data[-1][ticker]["close"]
 
-            losing_for_5_days =  ohlcv_data[-6][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
-                                 ohlcv_data[-5][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
+            losing_for_5_days =  ohlcv_data[-6][ticker]["close"] > ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] > ohlcv_data[-4][ticker]["close"] and \
                                  ohlcv_data[-4][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
                                  ohlcv_data[-3][ticker]["close"] > ohlcv_data[-2][ticker]["close"] and \
                                  ohlcv_data[-2][ticker]["close"] > ohlcv_data[-1][ticker]["close"]
             
+            gaining_for_6_days = ohlcv_data[-7][ticker]["close"] < ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] < ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] < ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] < ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] < ohlcv_data[-1][ticker]["close"]
+
+            losing_for_6_days =  ohlcv_data[-7][ticker]["close"] > ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] > ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] > ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] > ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] > ohlcv_data[-1][ticker]["close"]
+                        
+            gaining_for_7_days = ohlcv_data[-8][ticker]["close"] < ohlcv_data[-7][ticker]["close"] and \
+                                 ohlcv_data[-7][ticker]["close"] < ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] < ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] < ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] < ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] < ohlcv_data[-1][ticker]["close"]
+
+            losing_for_7_days =  ohlcv_data[-8][ticker]["close"] > ohlcv_data[-7][ticker]["close"] and \
+                                 ohlcv_data[-7][ticker]["close"] > ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] > ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] > ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] > ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] > ohlcv_data[-1][ticker]["close"]
+                        
+            gaining_for_8_days = ohlcv_data[-9][ticker]["close"] < ohlcv_data[-8][ticker]["close"] and \
+                                 ohlcv_data[-8][ticker]["close"] < ohlcv_data[-7][ticker]["close"] and \
+                                 ohlcv_data[-7][ticker]["close"] < ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] < ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] < ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] < ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] < ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] < ohlcv_data[-1][ticker]["close"]
+
+            losing_for_8_days =  ohlcv_data[-9][ticker]["close"] > ohlcv_data[-8][ticker]["close"] and \
+                                 ohlcv_data[-8][ticker]["close"] > ohlcv_data[-7][ticker]["close"] and \
+                                 ohlcv_data[-7][ticker]["close"] > ohlcv_data[-6][ticker]["close"] and \
+                                 ohlcv_data[-6][ticker]["close"] > ohlcv_data[-5][ticker]["close"] and \
+                                 ohlcv_data[-5][ticker]["close"] > ohlcv_data[-4][ticker]["close"] and \
+                                 ohlcv_data[-4][ticker]["close"] > ohlcv_data[-3][ticker]["close"] and \
+                                 ohlcv_data[-3][ticker]["close"] > ohlcv_data[-2][ticker]["close"] and \
+                                 ohlcv_data[-2][ticker]["close"] > ohlcv_data[-1][ticker]["close"]
+                        
             # If the stock is currently being held, sell it (set allocation to 0) no matter what
             if self.stock_holdings[ticker]:
                 allocation_dict[ticker] = 0
@@ -49,7 +98,7 @@ class TradingStrategy(Strategy):
             #elif gaining_for_5_days:
             #    allocation_dict[ticker] = 1  # Allocate all to this stock
             #    self.stock_holdings[ticker] = True  # Update the holding status
-            elif gaining_for_5_days:
+            elif losing_for_8_days:
                 allocation_dict[ticker] = 1  # Allocate all to this stock
                 self.stock_holdings[ticker] = True  # Update the holding status
             else:
