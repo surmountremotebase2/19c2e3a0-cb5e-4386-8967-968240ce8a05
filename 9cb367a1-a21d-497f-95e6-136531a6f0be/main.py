4f69179786699e6aa2f3d64d1725c180092a725d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-16 14:00:34 -0400

    User Craig Woody saved changes to main.py at 03/16/2024, 18:00:33 UTC time.

--- 9cb367a1-a21d-497f-95e6-136531a6f0be/main.py
+++ 9cb367a1-a21d-497f-95e6-136531a6f0be/main.py
@@ -6,7 +6,7 @@ class TradingStrategy(Strategy):
 
     @property
     def assets(self):
-        return ["SQ", "GOVT"]
+        return ["TSLA", "TSLS"]
 
     @property
     def interval(self):
@@ -19,24 +19,24 @@ class TradingStrategy(Strategy):
         BP_stake = 0
         F_stake = 0
 
-        F_bbands = BB("SQ", data, 20, 1.4)
-        F_ma = SMA("SQ", data, 5)
+        F_bbands = BB("TSLA", data, 20, 1.4)
+        F_ma = SMA("TSLA", data, 5)
 
         if len(data)<20:
             return TargetAllocation({})
 
-        current_price = data[-1]["SQ"]['close']
+        current_price = data[-1]["TSLA"]['close']
 
         if F_bbands is not None and current_price < F_bbands['lower'][-1] and F_ma[-1]>F_ma[-2]:
             log("going long")
-            if holdings["SQ"] >= 0:
-                F_stake = min(1, holdings["SQ"]+0.1)
+            if holdings["TSLA"] >= 0:
+                F_stake = min(1, holdings["TSLA"]+0.1)
             else:
                 F_stake = 0.4
         elif F_bbands is not None and current_price > F_bbands['upper'][-1]:
             log("going short")
-            if holdings["GOVT"] >= 0:
-                BP_stake = min(1, holdings["GOVT"]+0.075)
+            if holdings["TSLS"] >= 0:
+                BP_stake = min(1, holdings["TSLS"]+0.075)
             else:
                 BP_stake = 0.2
         else:
@@ -44,4 +44,4 @@ class TradingStrategy(Strategy):
             F_stake = 0
             BP_stake = 0
 
-        return TargetAllocation({"GOVT": BP_stake, "SQ": F_stake})
+        return TargetAllocation({"TSLS": BP_stake, "TSLA": F_stake})
