cffa14b2668b119b768c04b4382c1d21f088f8ce
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-18 22:00:49 -0400

    User Craig Woody saved changes to main.py at 03/19/2024, 02:00:48 UTC time.

--- e2f36c6d-fc2c-4929-b4e4-59b0424206ab/main.py
+++ e2f36c6d-fc2c-4929-b4e4-59b0424206ab/main.py
@@ -1,1 +1,24 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        # Define the ticker symbol for the tech company we're interested in
+        self.ticker = "AAPL"
+
+    @property
+    def assets(self):
+        # Indicate which assets this strategy will trade. In our case, just AAPL.
+        return [self.ticker]
+
+    @property
+    def interval(self):
+        # Define the interval for running this strategy. We'll run it daily.
+        return "1day"
+
+    def run(self, data):
+        # Define the target allocation for AAPL. In this example, we allocate 100% of the portfolio to AAPL.
+        # This means every day, the strategy will try to ensure all available funds are used to buy AAPL shares.
+        allocation_dict = {self.ticker: 1.0}
+        
+        # Return the target allocation as a TargetAllocation object.
+        return TargetAllocation(allocation_dict)
